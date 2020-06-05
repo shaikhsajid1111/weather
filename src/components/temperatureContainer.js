51310f348f70790f9f5bc986224e0d6d736b19a4
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2020-06-05 12:20:29 +0530

    added seperate folders for components and CSS for each components

---
+++ src/components/temperatureContainer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import '../assets/css/temperatureContainer.css';
+
+
+
+
+const TemperatureContainer= (props) =>{
+    var date = new Date(Number(props.date));
+    var fullDate = date.toDateString();
+    return(<div>
+        <h1 className="text-center catamaran">{props.city},{props.country}</h1>
+    <h3 className='rubik'>{fullDate}</h3>
+        <div className="container">
+
+            
+            <h1 id="temperature">{props.temp}<sup>°C</sup></h1>
+            
+        </div>
+        <div>
+        <h2 className="text-center catamaran">{props.main}</h2>
+        </div>
+        <div>
+    <h2 className="text-center catamaran">{props.temp_min}<sup>°C</sup>/{props.temp_max}<sup>°C</sup></h2>
+        </div>      
+    </div>
+    )}
+
+export default TemperatureContainer;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './assets/css/index.css';
+import TemperatureContainer from './components/temperatureContainer';
+require('dotenv').config();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+/*parnts component*/
+ class App extends React.Component{
+    
+    constructor(props){
+        super(props) ;
+        
+        this.state = {
+            items : [],
+            isLoaded : false,
+        }
+     }
+     //75fdb2c2a64ac715c062a11eb71a7353
+     componentDidMount(){
+        //https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={your api key}
+         fetch(`https://something.`)
+        .then(res => res.json())
+        .then(json => {
+            this.setState(
+                {
+                    isLoaded : true,
+                    items : json
+                }
+            )
+        })
+        }
+
+     render(){
+         var {isLoaded,items} = this.state;
+         if(!isLoaded)          /*remove exclamation after development*/ 
+            return(
+                <div>
+                    <TemperatureContainer 
+                    city = "Mumbai"
+                    temp = '26'
+                    main='Drizzle'
+                    description = "light intensity drizzle"
+                    pressure = "1012"
+                    humidity = "81"
+                    temp_min = '289'
+                    temp_max = '355'
+                    visibility = '10000'
+                    wind_speed = '125'
+                    wind_deg = '45'
+                    country = "India"
+                    rain = "Heavy"
+                    snow = "Heavy"
+                    cloud = "Ssss"
+                    date = '1485792967'
+                    />
+                
+                 </div>
+                
+                 )
+        return (
+            <div>
+            
+            </div>
+        ) 
+        
+         
+     }
+        
+     
+
+
+ }
+
+ /*
+ {lat, lon, alt_m, alt_ft, wx_desc, wx_code, wx_icon, temp_c, temp_f, feelslike_c, feelslike_f, humid_pct, windspd_mph,
+     windspd_kmh, windspd_kts, windspd_ms,
+     winddir_deg, winddir_compass, 
+     cloudtotal_pct, vis_km, vis_mi, vis_desc, slp_mb, slp_in, dewpoint_c, dewpoint_f})
+ */ 
+ReactDOM.render(<App/>,document.getElementById('root'));
